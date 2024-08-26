4ac3cd734ef03dad8e198911999a09f750cb910b
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-08-26 12:41:54 +0530

    Defining a Mutation Schema and Adding Mutation Resolver

--- Back End(Node)/graphql/resolvers.js
+++ Back End(Node)/graphql/resolvers.js
@@ -1,5 +1,20 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 module.exports = {
-  hello() {
-    return { text: "Hello World!", views: 12345 };
+  createUser: async function ({ userInput }, req) {
+    const { email, name, password } = userInput;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error(`User ${name} already exists`);
+      throw error;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ email, name, password: hashedPassword });
+    const createdUser = await newUser.save();
+
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
 };
