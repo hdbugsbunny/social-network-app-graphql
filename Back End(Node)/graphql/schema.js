8a838487bdb9d8a1b4a95a9100d22376f0ba409e
Harshit Durgapal <harshit.durgapal@mobcoder.com>
2024-08-27 17:08:24 +0530

    Adding a Get Post Query & Resolver

--- Back End(Node)/graphql/schema.js
+++ Back End(Node)/graphql/schema.js
@@ -25,6 +25,11 @@ module.exports = buildSchema(`
         userId: String!
     }
     
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
+    }
+    
     input UserInputData {
         email: String!
         name: String!
@@ -39,6 +44,7 @@ module.exports = buildSchema(`
     
     type RootQuery {
         loginUser(email: String!, password: String!): AuthData
+        userPosts: PostData!
     }
     
     type RootMutation {
